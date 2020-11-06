8eb7b6540bd2dd3ee055b92af4e567ee6452cd7d
David Fernández Ramos <dfernandez@fluendo.com>
2020-11-06 16:16:45 +0100

    Add MVVMCross UI project

---
+++ TipCalc.Forms.UI/FormsApp.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace TipCalc.Forms.UI
+{
+    public partial class FormsApp : Application
+    {
+        public FormsApp()
+        {
+            InitializeComponent();
+        }
+    }
+}
