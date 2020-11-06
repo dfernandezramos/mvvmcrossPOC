9b6e87a5fcbfdd1fd9359ff2347e9db98776a77b
David Fernández Ramos <dfernandez@fluendo.com>
2020-11-06 16:18:59 +0100

    Add MVVMCross Android project

---
+++ TipCalc/TipCalc.Android/MainActivity.cs
@@ -0,0 +1,22 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using MvvmCross.Forms.Platforms.Android.Core;
+using MvvmCross.Forms.Platforms.Android.Views;
+using TipCalc.Forms.UI;
+
+namespace TipCalc.Android
+{
+    [Activity(Label = "TipCalc", Theme = "@style/MainTheme", MainLauncher = true,
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : MvxFormsAppCompatActivity<MvxFormsAndroidSetup<TipCalc.Core.App, FormsApp>, TipCalc.Core.App, FormsApp>
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+        }
+    }
+}
